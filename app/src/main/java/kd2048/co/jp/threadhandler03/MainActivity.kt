5513092b60a7213a6cc312c2dd0b84a17a929f6d
Akira Kobayashi <kuppy777@ari.bekkoame.ne.jp>
2020-05-20 08:54:40 +0900

    Runnableをpostとメッセージpost

---
+++ app/src/main/java/kd2048/co/jp/threadhandler03/MainActivity.kt
@@ -0,0 +1,52 @@
+package kd2048.co.jp.threadhandler03
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.os.Message
+import android.util.Log
+import android.view.WindowManager
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.concurrent.thread
+
+class MainActivity : AppCompatActivity() {
+
+    private val handler :Handler = object : Handler(Looper.getMainLooper()){
+        override fun handleMessage(msg: Message?) {
+            super.handleMessage(msg)
+            when(msg?.what) {
+                1 -> {
+                    Log.d("tetsu", msg.obj.toString())
+                }
+            }
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+
+        val text = findViewById<TextView>(R.id.text)
+
+        thread{
+            Log.d("tetsu", "${Thread.currentThread()} thread sleep")
+            Thread.sleep(10000)
+            val r = Runnable {
+                Log.d("tetsu", "${Thread.currentThread()} Runnable")
+                val str = "RunnnaableをUIスレッドで実行して表示更新"
+                text.setText(str)
+            }
+            handler.post(r)
+        }
+
+        thread{
+            Log.d("tetsu", "${Thread.currentThread()} Message thread")
+            Thread.sleep(6000)
+            val msg = handler.obtainMessage(1, "test")
+            handler.handleMessage(msg)
+        }
+    }
+}
